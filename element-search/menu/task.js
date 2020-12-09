2532e07c67af853ff72528eb3193b6c9397a43c8
Ekaterina Volkova <volkovakaterin@gmail.com>
2020-12-09 15:11:20 +0400

    menu changed

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,12 @@
-  const links = document.querySelectorAll('.menu__link');
-  const menuItem = document.querySelectorAll('.menu__item');
+const links = document.querySelectorAll('.menu__link');
+const menuItem = document.querySelectorAll('.menu__item');
 links.forEach(link => {
-link.addEventListener('click', () => {
-  menuSub = link.parentElement.querySelector('.menu_sub');
-  if(menuSub !== null) {
-  menuSub.classList.add('menu_active');
-  link.preventDefault();
-  }
-})
+    link.addEventListener('click', (event) => {
+        menuSub = link.parentElement.querySelector('.menu_sub');
+        if (menuSub !== null) {
+            event.preventDefault();
+            menuSub.classList.add('menu_active');
+
+        }
+    })
 });
